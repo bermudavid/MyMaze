cb82dce27bfe4cce67eea120fac890c7e1346c5f
David Martín <bermudavid2@gmail.com>
2017-07-14 01:13:58 +0100

    Maze 3 finished
    deleted recursive solution
    added sleep time to see the path followed
    added O character to see the backtrackings
    added method to clean the backtracking characters

--- src/mazeproject/MyMaze.java
+++ src/mazeproject/MyMaze.java
@@ -5,6 +5,7 @@ import java.io.FileReader;
 import java.io.IOException;
 import java.io.PrintWriter;
 import java.io.UnsupportedEncodingException;
+import static java.lang.Thread.sleep;
 import java.util.LinkedList;
 import java.util.logging.Level;
 import java.util.logging.Logger;
@@ -18,12 +19,13 @@ public class MyMaze {
     // F for finish
     // X for wall
     // . for open space
+    // O for backtracking
   
   private static final String FILENAME = "input_file";
   //map 
   LinkedList<LinkedList<Character>> map = new LinkedList<>();
-   java.awt.Point start = new java.awt.Point();
-   
+    java.awt.Point start = new java.awt.Point();
+    
     public static void main(String[] args) {
       new MyMaze();
     }
@@ -56,47 +58,236 @@ public class MyMaze {
       }
       return copy;
     }
-    //test
-    int counter = 0;
-    public boolean step(int x, int y){
-      counter ++;
-      // dead end
-      if(x < 0 || y < 0 || x >= map.size() || y >= map.get(0).size()){
-        printMaze(map);
-        return false;
-      }
-      //if are ok, finish
-      if(map.get(x).get(y) == 'F'){
-        map.get(x).remove(y);
-        map.get(x).add(y,'f'); // to see 
-        return true;
-      }
-      //if is a wall or are visited square
-      if (map.get(x).get(y) == 'X' || map.get(x).get(y) == 'V') {
-			return false;
-		}
-      // maze step
-      map.get(x).set(y,'V');
-      boolean result = false;
-      //east
-      result = step(x,y+1);
-      if(result){return true;}
-      //north
-      result = step(x+1,y);
-      if(result){return true;}
-      //west 
-      result = step(x,y-1);
-      if(result){return true;}
-      //south
-      result = step(x-1,y);
-      if(result){return true;}
-      //dead end
+    /**
+     * mark with character
+     * @param x  
+     * @param y  
+     * @return 
+     */
+    void mark(int x, int y, Character v){
       map.get(x).remove(y);
-      map.get(x).add(y,'.');
-      //go back
+      map.get(x).add(y,v);
+      printMaze(map);
+    try {
+      sleep(150);
+    } catch (InterruptedException ex) {
+      Logger.getLogger(MyMaze.class.getName()).log(Level.SEVERE, null, ex);
+    }
+    }
+    void mark(int x, int y, Character v, LinkedList<LinkedList<Character>> maze){
+      maze.get(x).remove(y);
+      maze.get(x).add(y,v);
+      //printMaze(map);
+    }
+    boolean canMove(int x, int y){
+        if(x >= 0 && x < map.size() && y >= 0 && y < map.get(0).size()){
+          if(map.get(x).get(y) == '.'){
+            return true;
+          }
+        }
       return false;
     }
+    /**
+     * this represent a square
+     */
+    class Node {
+      private Character square;
+      private int x,y;
+      
+      Node (int x, int y, Character square){
+        this.x = x;
+        this.y = y;
+        this.square = square;
+      }
+      
+      public void setSquare(Character square){
+        this.square = square;
+      }
+      Character getSq(){
+        return this.square;
+      }
+
+      public int getX() {
+        return x;
+      }
+
+      public void setX(int x) {
+        this.x = x;
+      }
+
+      public int getY() {
+        return y;
+      }
+
+      public void setY(int y) {
+        this.y = y;
+      }
+      public Node north(){
+        if(isInMaze(x,y-1)){
+          return new Node(x,y-1,map.get(x).get(y-1));
+        } else {
+          return this;
+        }
+      }
+      public Node east(){
+        if(isInMaze(x+1,y)){
+          return new Node(x+1,y,map.get(x+1).get(y));
+        } else {
+          return this;
+        }
+      }
+      public Node south(){
+        if(isInMaze(x,y+1)){
+          return new Node(x,y+1,map.get(x).get(y+1));
+        } else {
+          return this;
+        }
+      }
+      public Node west(){
+        if(isInMaze(x-1,y)){
+          return new Node(x-1,y,map.get(x-1).get(y));
+        } else {
+          return this;
+        }
+      }
+    }
+    /**
+     * check if the square is reachable
+     * @param squ char to probe
+     * @return true if is reachable false is not
+     */
+    boolean charValid(Character squ){
+      if( squ == '.' || squ == 'F' ){
+        return true;
+      } else {
+        return false;
+      }
+    }
+    boolean haveNeigs(Node n){
+      if(isClear(n.north())){
+        return true;
+      }
+      if(isClear(n.south())){
+        return true;
+      }
+      if(isClear(n.east())){
+        return true;
+      }
+      if(isClear(n.west())){
+        return true;
+      }
+      return false;
+    }
+    /**
+     * Check is a concrete position is in maze and if is open space
+     * @param i - x position
+     * @param j - y position
+     * @return true if is open space and is in maze
+     */
+    public boolean isClear(int i, int j) {
+      assert(isInMaze(i,j)); 
+      return (map.get(i).get(j) != 'X' && map.get(i).get(j) != 'V' && map.get(i).get(j) != 'O');
+    }
+    /**
+     * overload to check node
+     * @param pos - node to check
+     * @return true if is open space and is in maze
+     */
+    public boolean isClear(Node pos) {
+      return isClear(pos.getX(), pos.getY());
+    }
+
+    /**
+     * check if position is in maze
+     * @param i - x position
+     * @param j - y position
+     * @return true if cell is within maze
+     */ 
+    public boolean isInMaze(int i, int j) {
+      if (i >= 0 && i<map.size() && j>= 0 && j<map.get(0).size()) return true; 
+      else return false;
+    }
+      
+    /**
+     * return if a node is in maze
+     * @param pos - node to check
+     * @return true if node is within maze
+     */
+    public boolean isInMaze(Node pos) {
+      if(pos == null){
+        return false;
+      }
+      return isInMaze(pos.getX(), pos.getY());
+    }
     
+    /**
+     * Solve maze by stack backtracing method
+     * @param x position of node start
+     * @param y position of node start
+     * @return true if reach end false if not
+     */
+    public boolean solveStack(int x, int y){
+      LinkedList<Node> S = new LinkedList<>(); // stack
+      Node v = new Node(x,y,map.get(x).get(y)); // add initial pos
+      Node next;
+      S.push(v);
+      
+      while(!S.isEmpty()){
+        v = S.peekLast();  
+        if(v.getSq() == 'F') {
+          mark(v.getX(),v.getY(),'V');
+          v.setSquare('V');
+          break;
+        }
+        //mark as visited
+        mark(v.getX(),v.getY(),'V');
+        v.setSquare('V');
+        c:if(haveNeigs(v)){ // have neighborgs
+          //mar as visited
+          
+          // use break to take only one
+          next  = v.south();
+          if(isInMaze(next) && isClear(next)){
+            S.add(next);
+            break c;
+          }
+          next  = v.east();
+          if(isInMaze(next) && isClear(next)){
+            S.add(next);
+            break c;
+          }
+          next  = v.north();
+          if(isInMaze(next) && isClear(next)){
+            S.add(next);
+            break c;
+          }
+          next  = v.west();
+          if(isInMaze(next) && isClear(next)){
+            S.add(next);
+            break c;
+          }   
+        } else {
+          mark(v.getX(),v.getY(),'O');
+          v.setSquare('O');
+          S.removeLast();
+        }
+      }
+      //restore maz & show path
+      removeMazeBacktracing(map);
+      System.out.println("Path:");
+      printMaze(map);
+      return true;
+    }
+    
+    void removeMazeBacktracing(LinkedList<LinkedList<Character>>map){
+      for (int i = 0; i < map.size(); i++) {
+        for (int j = 0; j < map.get(0).size(); j++) {
+          if(map.get(i).get(j) == 'O'){
+            mark(i,j,'.',map);
+          }
+        }
+      }
+    }
     
     /**
      * solve the maze
@@ -104,11 +295,12 @@ public class MyMaze {
      * @param y - end.
      */
     public void solveMaze(int x, int y) {
-      if(step(x,y)){
+      /*if(step(x,y)){
         map.get(y).remove(x);
         map.get(y).add(x,'S');
         printMaze(map);
-      }
+      }*/
+      solveStack(x,y);
     }
     public int[] findInit(){
       int[] res = {0,0};
@@ -279,9 +471,8 @@ public class MyMaze {
       int[] init = findInit();
       int x = init[0];
       int y = init[1];
-      solveMaze(0,0);
+      solveMaze(x,y);
       printPreMaze();
-      
     }
 
 }
